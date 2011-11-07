50cdaad1d45b275c3c115b7d05b06294e8c53a02
Javier Martin de Valmaseda <martin9sek@gmail.com>
2011-11-07 13:13:56 +0100

    4.4 finished, we need to check useTexture in diffuse.fs

--- src/exercises/02-SolarViewer/diffuse.fs
+++ src/exercises/02-SolarViewer/diffuse.fs
@@ -12,8 +12,17 @@ void main()
 	//Exercise 4.4: Calculate the reflected intensities for the direct sun light (using lightDir and lightcolor) 
 	//and indirect light (using indirectLightDir and indirectlightcolor)
 		
-	vec3 color = lightcolor * diffuseColor * dot(normal,lightDir)*useTexture;
-	vec3 indcolor = indirectlightcolor * diffuseColor * dot(normal,indirectLightDir)*useTexture;
+	vec3 color = vec3(0,0,0);
+	vec3 indcolor = vec3(0,0,0);
+	//if (useTexture==1)
+		//diffuseColor= 
+	
+	if (dot(normal,lightDir)>0)
+		color = lightcolor * diffuseColor * dot(normal,lightDir);
+	 
+	if (dot(normal,indirectLightDir)>0)
+		indcolor = indirectlightcolor * diffuseColor * dot(normal,indirectLightDir);
+	
 	vec4 finalcolor = vec4(color, 1.0) + vec4(indcolor, 1.0);
 	
 	gl_FragColor = texture2D(texture, gl_TexCoord[0].xy) * finalcolor;
